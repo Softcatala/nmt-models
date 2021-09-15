e232eaad6ef258c53bbd74d262de65e8c0ea9c7e
Jordi Mas <jmas@softcatala.org>
2021-09-15 19:08:39 +0200

    Organize better the directories

--- languages/get-corpuses.sh
+++ languages/get-corpuses.sh
@@ -2,17 +2,18 @@
 
 git clone https://github.com/Softcatala/parallel-catalan-corpus corpus-raw
 cd corpus-raw
-git checkout new-corpus
+#git checkout new-corpus
 
 declare -a arr=("eng-cat" "deu-cat" "ita-cat" "fra-cat" "spa-cat" "nld-cat")
 
 for dirname in "${arr[@]}"; do
     echo Copying $dirname
-    rm -f -r ../$dirname/corpus-raw/
-    mkdir -p ../$dirname/corpus-raw/
-    cp -r $dirname/* ../$dirname/corpus-raw/
+    target_dir=../$dirname/corpus-raw/
+    rm -f -r $target_dir
+    mkdir -p $target_dir
+    cp -r $dirname/* $target_dir
     pushd .
-    cd ../$dirname/corpus-raw/
+    cd $target_dir
     if compgen -G "*.xz" > /dev/null; then
          xz -f -d *.xz
     fi
