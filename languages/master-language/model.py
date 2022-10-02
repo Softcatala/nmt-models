41454bfd0d84b15c606081be77d1cb167158650c
Jordi Mas <jmas@softcatala.org>
2022-10-02 15:52:28 +0200

    All

---
+++ languages/master-language/model.py
@@ -0,0 +1,7 @@
+class Softcatala(transformer.Transformer):
+    """Defines a base Transformer model using relative position representations as
+    described in https://arxiv.org/abs/1803.02155.
+    """
+
+    def __init__(self):
+        super().__init__(position_encoder_class=None, maximum_relative_position=20, share_embeddings=sequence_to_sequence.EmbeddingsSharingLevel.AUTO)
