69b41ac5de1e721f34b04a6ded51fdb2b44bb784
Jordi Mas <jmas@softcatala.org>
2022-11-04 18:08:19 +0100

    Install early on all deps needed

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -1,5 +1,6 @@
+sudo sudo apt-get update -y && sudo apt-get install python3-pip zip -y
+
 pip3 install -r requirements.txt
-sudo apt-get install zip -y
 
 git config --global user.name "Jordi Mas"
 git config --global user.email jmas@softcatala.org
