831962fa63df930ae018d338ce4f9afd270b3740
Jordi Mas <jmas@softcatala.org>
2022-11-24 16:57:50 +0100

    Fix

--- languages/experiments.sh
+++ languages/experiments.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+
+echo Setup experiments
 cp -r eng-cat eng-cat-v1
 cp -r eng-cat eng-cat-v2
 cp -r eng-cat eng-cat-v3
 cp -r eng-cat eng-cat-v4
 cp -r eng-cat eng-cat-v5
+
+echo Copy single voc.sh
+cp master-language/voc.sh eng-cat-v1/
+cp master-language/voc.sh eng-cat-v2/
+cp master-language/voc.sh eng-cat-v3/
+cp master-language/voc.sh eng-cat-v4/
+cp master-language/voc.sh eng-cat-v5/
+
+echo Setup voc.sh for experiments
+sed -i 's/50000/10000/g' eng-cat-v1/voc.sh
+sed -i 's/50000/20000/g' eng-cat-v2/voc.sh
+sed -i 's/50000/30000/g' eng-cat-v3/voc.sh
+sed -i 's/50000/40000/g' eng-cat-v4/voc.sh
+#sed -i 's/50000/50000/g' eng-cat-v5/voc.sh
 
