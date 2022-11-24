1bc6bfe36f873632ff9e2c31e65e489615b3271c
Jordi Mas <jmas@softcatala.org>
2022-11-24 17:12:43 +0100

    Fix

--- languages/experiments.sh
+++ languages/experiments.sh
@@ -8,16 +8,33 @@ cp -r eng-cat eng-cat-v4
 cp -r eng-cat eng-cat-v5
 
 echo Copy single voc.sh
-cp master-language/voc.sh eng-cat-v1/
-cp master-language/voc.sh eng-cat-v2/
-cp master-language/voc.sh eng-cat-v3/
-cp master-language/voc.sh eng-cat-v4/
-cp master-language/voc.sh eng-cat-v5/
+rm eng-cat-v1/voc.sh && cp master-language/voc.sh eng-cat-v1/
+rm eng-cat-v2/voc.sh && cp master-language/voc.sh eng-cat-v2/
+rm eng-cat-v3/voc.sh && cp master-language/voc.sh eng-cat-v3/
+rm eng-cat-v4/voc.sh && cp master-language/voc.sh eng-cat-v4/
+rm eng-cat-v5/voc.sh && cp master-language/voc.sh eng-cat-v5/
 
 echo Setup voc.sh for experiments
 sed -i 's/50000/10000/g' eng-cat-v1/voc.sh
 sed -i 's/50000/20000/g' eng-cat-v2/voc.sh
 sed -i 's/50000/30000/g' eng-cat-v3/voc.sh
 sed -i 's/50000/40000/g' eng-cat-v4/voc.sh
-#sed -i 's/50000/50000/g' eng-cat-v5/voc.sh
+sed -i 's/50000/50000/g' eng-cat-v5/voc.sh
 
+expoert QUICK_PREPROCESSING=1
+cd eng-cat-v1 && ./voc.sh && ./preprocess.sh && ./train.sh && ./export.sh
+cd eng-cat-v2 && ./voc.sh && ./preprocess.sh && ./train.sh && ./export.sh
+cd eng-cat-v3 && ./voc.sh && ./preprocess.sh && ./train.sh && ./export.sh
+cd eng-cat-v4 && ./voc.sh && ./preprocess.sh && ./train.sh && ./export.sh
+cd eng-cat-v5 && ./voc.sh && ./preprocess.sh && ./train.sh && ./export.sh
+
+echo "Experiment 1" > results.txt
+cat eng-cat-v1/bleu.txt >> results.txt
+echo "Experiment 2" > results.txt
+cat eng-cat-v2/bleu.txt >> results.txt
+echo "Experiment 3" > results.txt
+cat eng-cat-v3/bleu.txt >> results.txt
+echo "Experiment 4" > results.txt
+cat eng-cat-v4/bleu.txt >> results.txt
+echo "Experiment 5" > results.txt
+cat eng-cat-v5/bleu.txt >> results.txt
