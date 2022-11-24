12edd0631c090ba5c206af74ea2ffc389201cd1c
Jordi Mas <jmas@softcatala.org>
2022-11-24 21:46:07 +0100

    Fix

--- languages/experiments.sh
+++ languages/experiments.sh
@@ -22,22 +22,22 @@ sed -i 's/50000/40000/g' eng-cat-v4/voc.sh
 sed -i 's/50000/50000/g' eng-cat-v5/voc.sh
 
 export QUICK_PREPROCESSING=1
-cd eng-cat-v1 && ./voc.sh && ./preprocess.sh && ./train.sh && ./export.sh && cd ..
+cd eng-cat-v1 && ./preprocess.sh && ./voc.sh && ./train.sh && ./export.sh && cd ..
 echo "Experiment 1" > results.txt
 cat eng-cat-v1/bleu.txt >> results.txt
 
-cd eng-cat-v2 && ./voc.sh && ./preprocess.sh && ./train.sh && ./export.sh && cd ..
+cd eng-cat-v2 && ./preprocess.sh && ./voc.sh && ./train.sh && ./export.sh && cd ..
 echo "Experiment 2" > results.txt
 cat eng-cat-v2/bleu.txt >> results.txt
 
-cd eng-cat-v3 && ./voc.sh && ./preprocess.sh && ./train.sh && ./export.sh && cd ..
+cd eng-cat-v3 && ./preprocess.sh && ./voc.sh && ./train.sh && ./export.sh && cd ..
 echo "Experiment 3" > results.txt
 cat eng-cat-v3/bleu.txt >> results.txt
 
-cd eng-cat-v4 && ./voc.sh && ./preprocess.sh && ./train.sh && ./export.sh && cd ..
+cd eng-cat-v5 && ./preprocess.sh && ./voc.sh && ./train.sh && ./export.sh && cd ..
 echo "Experiment 4" > results.txt
 cat eng-cat-v4/bleu.txt >> results.txt
 
-cd eng-cat-v5 && ./voc.sh && ./preprocess.sh && ./train.sh && ./export.sh && cd ..
+cd eng-cat-v5 && ./preprocess.sh && ./voc.sh && ./train.sh && ./export.sh && cd ..
 echo "Experiment 5" > results.txt
 cat eng-cat-v5/bleu.txt >> results.txt
