bf369d7c0d6b9fc519e7f62e8c69470a2a8cfbed
Jordi Mas <jmas@softcatala.org>
2022-11-25 19:10:06 +0100

    Fix

--- languages/experiment-upper.sh
+++ languages/experiment-upper.sh
@@ -17,7 +17,6 @@ echo Setup voc.sh for experiments
 sed -i 's/augmentation_cap: 100/augmentation_cap: 100/g' eng-cat-u1/corpus.yml
 sed -i 's/augmentation_cap: 100/augmentation_cap: 50/g' eng-cat-u2/corpus.yml
 
-exit
 export QUICK_PREPROCESSING=1
 cd eng-cat-u1 && ./preprocess.sh && ./voc.sh && ./train.sh && ./export.sh && cd ..
 echo "Experiment 1" > results-upper.txt
