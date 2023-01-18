b7eec2bb142a9e41be3b7a0bd4bc88d5015a6525
Jordi Mas <jmas@softcatala.org>
2023-01-18 16:04:59 +0100

    Fixes

--- languages/experiment-run-g1.sh
+++ languages/experiment-run-g1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo Run experiments
+echo Run experiments spa-cat
 
 cd spa-cat-e1 && ./voc.sh && ./train.sh && ./export.sh && cd ..
 echo "Experiment 1" > results-g2-spa-cat.txt
@@ -13,4 +13,18 @@ cat spa-cat-e2/bleu.txt >> results-g2-spa-cat.txt
 cd spa-cat-e3 && ./voc.sh && ./train.sh && ./export.sh && cd ..
 echo "Experiment 3" >> results-g2-spa-cat.txt
 cat spa-cat-e3/bleu.txt >> results-g2-spa-cat.txt
+
+echo Run experiments cat-spa
+
+cd cat-spa-e1 && ./voc.sh && ./train.sh && ./export.sh && cd ..
+echo "Experiment 1" > results-g2-cat-spa.txt
+cat cat-spa-e1/bleu.txt >> results-g2-cat-spa.txt
+
+cd cat-spa-e2 && ./voc.sh && ./train.sh && ./export.sh && cd ..
+echo "Experiment 2" >> results-g2-cat-spa.txt
+cat cat-spa-e2/bleu.txt >> results-g2-cat-spa.txt
+
+cd cat-spa-e3 && ./voc.sh && ./train.sh && ./export.sh && cd ..
+echo "Experiment 3" >> results-g2-cat-spa.txt
+cat cat-spa-e3/bleu.txt >> results-g2-cat-spa.txt
 
