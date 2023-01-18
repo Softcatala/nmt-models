5878865653090e09cb6722ecba8fc16e1e5f3837
Jordi Mas <jmas@softcatala.org>
2023-01-18 16:58:44 +0100

    gpus

--- languages/spa-cat/train.sh
+++ languages/spa-cat/train.sh
@@ -1,2 +1,2 @@
 #--num_gpus 4 for -> multiple gpus
-onmt-main --mixed_precision --model BigTransformer --config data.yml --auto_config train --with_eval
+onmt-main --mixed_precision --model BigTransformer --config data.yml --auto_config train --with_eval --num_gpus 4
