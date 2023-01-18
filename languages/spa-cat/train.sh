bd003f2054bc5cb63734d41abdfee3bd11bd6ccc
Jordi Mas <jmas@softcatala.org>
2023-01-18 18:24:22 +0100

    Fix

--- languages/spa-cat/train.sh
+++ languages/spa-cat/train.sh
@@ -1,2 +1,2 @@
 #--num_gpus 4 for -> multiple gpus
-onmt-main --mixed_precision --model TransformerBigRelative --config data.yml --auto_config train --with_eval --num_gpus 4
+onmt-main --mixed_precision --model_type TransformerBigRelative --config data.yml --auto_config train --with_eval --num_gpus 4
