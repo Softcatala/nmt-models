cce2963cd8848a220c9d94f67afb8a5c16b5bddb
Jordi Mas <jmas@softcatala.org>
2023-01-18 17:22:08 +0100

    One dir

--- languages/experiment-run-g1.sh
+++ languages/experiment-run-g1.sh
@@ -14,17 +14,4 @@ cd spa-cat-e3 && ./voc.sh && ./train.sh && ./export.sh && cd ..
 echo "Experiment 3" >> results-g2-spa-cat.txt
 cat spa-cat-e3/bleu.txt >> results-g2-spa-cat.txt
 
-echo Run experiments cat-spa
-
-cd cat-spa-e1 && ./voc.sh && ./train.sh && ./export.sh && cd ..
-echo "Experiment 1" > results-g2-cat-spa.txt
-cat cat-spa-e1/bleu.txt >> results-g2-cat-spa.txt
-
-cd cat-spa-e2 && ./voc.sh && ./train.sh && ./export.sh && cd ..
-echo "Experiment 2" >> results-g2-cat-spa.txt
-cat cat-spa-e2/bleu.txt >> results-g2-cat-spa.txt
-
-cd cat-spa-e3 && ./voc.sh && ./train.sh && ./export.sh && cd ..
-echo "Experiment 3" >> results-g2-cat-spa.txt
-cat cat-spa-e3/bleu.txt >> results-g2-cat-spa.txt
 
