311ed5ccdd48ee7c062593211b8235fe8853e23f
Jordi Mas <jmas@softcatala.org>
2023-01-26 16:53:02 +0100

    More

--- languages/experiment-run-g2.sh
+++ languages/experiment-run-g2.sh
@@ -2,7 +2,12 @@
 
 echo Run experiments spa-cat
 
-cd spa-cat-e4 && ./voc.sh && ./train.sh && ./export.sh && cd ..
-echo "Experiment 4" > results-g2-spa-cat.txt
-cat spa-cat-e4/bleu.txt >> results-g2-spa-cat.txt
+#cd spa-cat-e4 && ./voc.sh && ./train.sh && ./export.sh && cd ..
+#echo "Experiment 4" > results-g2-spa-cat.txt
+#cat spa-cat-e4/bleu.txt >> results-g2-spa-cat.txt
+
+
+cd spa-cat-e5 && ./voc.sh && ./train.sh && ./export.sh && cd ..
+echo "Experiment 5" > results-g2-spa-cat.txt
+cat spa-cat-e5/bleu.txt >> results-g2-spa-cat.txt
 
--- languages/experiment-setup-g2.sh
+++ languages/experiment-setup-g2.sh
@@ -5,14 +5,20 @@ export QUICK_PREPROCESSING=1
 # 2. https://huggingface.co/datasets/jordimas/spa-cat/tree/main expected
 
 cp -r spa-cat spa-cat-e4
+cp -r spa-cat spa-cat-e5
 
+#echo Setup experiment e4
+#cd spa-cat-e4
+#cp ../../../spa-cat/paracrawl.es-ca.* corpus-raw/
+#cp corpus-e4.yml corpus.yml 
+#./preprocess.sh
+#cd ..
 
-echo Setup experiment e4
-cd spa-cat-e4
+echo Setup experiment e5
+cd spa-cat-e5
 cp ../../../spa-cat/paracrawl.es-ca.* corpus-raw/
-cp corpus-e4.yml corpus.yml 
+cp corpus-e5.yml corpus.yml 
 ./preprocess.sh
 cd ..
-
 
 echo "Setup finished"
