488000ea59a852bed0d533d4386e05eced05c6b5
Jordi Mas <jmas@softcatala.org>
2024-08-08 19:09:44 +0200

    Fix

--- languages/preprocess-all.sh
+++ languages/preprocess-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a arr=("ita-cat" "fra-cat" "spa-cat" "por-cat" "eng-cat" "deu-cat" "nld-cat" "jpn-cat" "glg-cat" "oci-cat")
+declare -a arr=("ita-cat" "fra-cat" "spa-cat" "por-cat" "eng-cat" "deu-cat" "nld-cat" "jpn-cat" "glg-cat" "oci-cat" "eus-cat")
 
 for dirname in "${arr[@]}"; do
     echo Processing $dirname
