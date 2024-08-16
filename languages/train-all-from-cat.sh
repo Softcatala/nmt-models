d0a331951c0f67245654982819a0df7ac95c974c
Jordi Mas <jmas@softcatala.org>
2024-08-16 15:29:13 +0200

    Copy the model configuration from the corresponding pair instead of master-language

--- languages/train-all-from-cat.sh
+++ languages/train-all-from-cat.sh
@@ -4,7 +4,12 @@ declare -a arr=("cat-eng" "cat-deu" "cat-fra" "cat-ita" "cat-spa" "cat-por" "cat
 #declare -a arr=("cat-eng")
 
 for dirname in "${arr[@]}"; do
+
     echo Processing $dirname
+
+    src_lang="${dirname%-*}"  # Extract everything before the last '-'
+    tgt_lang="${dirname##*-}" # Extract everything after the last '-'
+    inverted_pair="$tgt_lang-$src_lang"
 
     if [ ! -d $dirname ]; then
         mkdir $dirname
@@ -12,8 +17,10 @@ for dirname in "${arr[@]}"; do
 
     pushd $dirname
 
-    cp ../master-language/* .
-    ./copy-corpus.sh
+    cp ../$inverted_pair/*.sh .
+    cp ../$inverted_pair/*.yml .
+    cp ../$inverted_pair/*.py .
+    ../master-language/copy-corpus.sh
     rm -r -f run/
     ./train.sh
     ./export.sh
